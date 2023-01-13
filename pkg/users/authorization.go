25c096416536abc2348aa0f2dc3b4ebec44b909e
Vishal <vishalrana9915@gmail.com>
2023-01-13 14:56:08 +0530

    ShouldBindJson was making the request body blank after use, so we need to use ioutil module and readAll function to read bytes and convert them into json using UnMarshal and after we are done with request , we need to use nopCloser function to reset the request body to its original value

--- pkg/users/authorization.go
+++ pkg/users/authorization.go
@@ -3,15 +3,22 @@ package users
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/vishalrana9915/demo_app/pkg/userInterface"
+	"github.com/vishalrana9915/demo_app/pkg/constant"
+	"github.com/vishalrana9915/demo_app/pkg/users/userInterface"
 )
 
 // a function handler to take care of user onboarding
 func RegisterUser(c *gin.Context) {
-	var payload userInterface.Users
-	json.Unmarshal(c.Request.Body, &payload)
-	fmt.Println(payload)
+	var body userInterface.Users
+	decode := json.NewDecoder(c.Request.Body)
+	if err := decode.Decode(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": constant.PARSING_ERROR})
+		c.Abort()
+		return
+	}
 
+	fmt.Println(body)
 }
